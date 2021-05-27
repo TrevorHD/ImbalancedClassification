809926de75ae78d9820807a1bced9697f6bf946b
TrevorHD <44789861+TrevorHD@users.noreply.github.com>
2021-05-27 17:42:34 -0400

    Miscellaneous fixes 21
    Updated the weighted accuracy scoring function to allow for more than two classes.

--- ImbalancedLearning.py
+++ ImbalancedLearning.py
@@ -23,7 +23,6 @@ from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 import numpy
 import warnings
 from pandas import DataFrame
-from numpy import sqrt
 from numpy import mean
 from numpy import argmax
 from numpy import arange
@@ -302,7 +301,7 @@ scorer = make_scorer(balanced_accuracy_score2, w1 = 1/100, w2 = 99/100)
 # Split data into training and test; fit model to training data, calculate weighted accuracy on test data
 # Repeat this n_repeats times with 50/50 test/train split
 # Accepts model or pipeline objects
-def model_cv_bal(obj, n_repeats, data_x = data_x, data_y = data_y, weights = [1] * 5000):
+def model_cv_bal(obj, n_repeats, data_x = data_x, data_y = data_y):
     cv = RepeatedStratifiedKFold(n_splits = 2, n_repeats = n_repeats, random_state = 32463)
     output = cross_validate(obj, data_x, data_y, cv = cv, scoring = scorer, n_jobs = -1)
     print(mean(output["test_score"]))
@@ -619,30 +618,38 @@ ax.text(11.7, -5.1, "Radial (c = 10)", fontsize = 4, horizontalalignment = "righ
 pyplot.tight_layout(pad = 0.4, w_pad = 1.2, h_pad = 1.0)
 pyplot.savefig("Plot_SVM2.jpeg", dpi = 800, facecolor = "white")
 
-# SVM will take quite a bit to run on this larger multi-class problem
-# Thus, we will oversample and undersample
-# This will rebalance classes and reduce the number of data points to work with
-os = RandomOverSampler(sampling_strategy = {3:1700})
-os_x, os_y = os.fit_resample(train_x, train_y)
-us = RandomUnderSampler(sampling_strategy = {0:200, 1:200, 2:200, 3:200})
-ous_x, ous_y = us.fit_resample(os_x, os_y)
-print(Counter(train_y))
-print(Counter(os_y))
-print(Counter(ous_y))
-
 # Compare models using cross-validation approach
 # For now, we are assuming that no class is more "important" than the other
 
 # Split data into training and test and fit SVM, then calculate stats on test data
 # Repeat this 1000 times with 50/50 test/train split
 # Do this for the radial kernels at various cost parameters
-model_cv(clfRC1, 1000, ["f1_micro", "f1_weighted"], ous_x, ous_y)
-model_cv(clfRC2, 1000, ["f1_micro", "f1_weighted"], ous_x, ous_y)
-model_cv(clfRC3, 1000, ["f1_micro", "f1_weighted"], ous_x, ous_y)
-model_cv(clfRC4, 1000, ["f1_micro", "f1_weighted"], ous_x, ous_y)
+model_cv(clfRC1, 1000, ["f1_micro", "f1_weighted"])
+model_cv(clfRC2, 1000, ["f1_micro", "f1_weighted"])
+model_cv(clfRC3, 1000, ["f1_micro", "f1_weighted"])
+model_cv(clfRC4, 1000, ["f1_micro", "f1_weighted"])
 
 # Again, we are assuming that no class is more "important" than the other
 # Thus, we can compare F1 micro average and find that c = 2 performs best
+
+# Redefine balanced_accuracy_score2 to allow for multiple class weights
+def balanced_accuracy_score3(y_true, y_pred, *, w1, w2, w3, w4, adjusted = False):
+    C = metrics.confusion_matrix(y_true, y_pred)
+    with numpy.errstate(divide = "ignore", invalid = "ignore"):
+        per_class = numpy.diag(C) / C.sum(axis = 1)
+    if numpy.any(numpy.isnan(per_class)):
+        warnings.warn("y_pred contains classes not in y_true")
+        per_class = per_class[~numpy.isnan(per_class)]
+    score = per_class[0]*w1 + per_class[1]*w2 + per_class[2]*w3 + per_class[3]*w4
+    if adjusted:
+        n_classes = len(per_class)
+        chance = 1 / n_classes
+        score -= chance
+        score /= 1 - chance
+    return score
+
+# Turn above function into scorer
+scorer = make_scorer(balanced_accuracy_score3, w1 = 1/100, w2 = 1/100, w3 = 1/100, w4 = 97/100)
 
 # Now, we assume that the "importance" of the minority class is approximately 100:1
 # A weighted model would incur a heavy penalty for misclassifying a minority class
@@ -650,13 +657,14 @@ model_cv(clfRC4, 1000, ["f1_micro", "f1_weighted"], ous_x, ous_y)
 # Split data into training and test and fit SVM, then calculate stats on test data
 # Repeat this 1000 times with 50/50 test/train split and 97:1:1:1 weight on minority class
 # Do this for the radial kernels at various cost parameters
-model_cv(clfLinC, 1000, ["balanced_accuracy"], ous_x, ous_y)
-model_cv(clf2dgC, 1000, ["balanced_accuracy"], ous_x, ous_y)
-model_cv(clf3dgC, 1000, ["balanced_accuracy"], ous_x, ous_y)
-model_cv(clfRbfC, 1000, ["balanced_accuracy"], ous_x, ous_y)
+# Reduce reps to 500 since this is more computationally expensive
+model_cv_bal(clfRC1C, 500)
+model_cv_bal(clfRC2C, 500)
+model_cv_bal(clfRC3C, 500)
+model_cv_bal(clfRC4C, 500)
 
 # Compare balanced accuracy, which inversely weights accuracy based on class size
-# We find that c = 5 performs best
+# We find that c = 1 performs best
 
 
 
